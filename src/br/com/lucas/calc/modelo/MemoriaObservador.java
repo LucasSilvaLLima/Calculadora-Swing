dc0f4a8f10c7159a5bda40ca0badd23a345fbfc0
Lucas Silva <luckassilvallima@hotmail.com>
2021-12-11 16:43:39 -0300

    Calculadora

---
+++ src/br/com/lucas/calc/modelo/MemoriaObservador.java
@@ -0,0 +1,6 @@
+package br.com.lucas.calc.modelo;
+
+public interface MemoriaObservador {
+
+	public void valorAlterado(String novoValor);
+}
---
+++ src/br/com/lucas/calc/visao/Botao.java
@@ -0,0 +1,21 @@
+package br.com.lucas.calc.visao;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class Botao extends JButton{
+
+	
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setFont(new Font("calculator", Font.PLAIN, 40));
+		setOpaque(true);
+		setBackground(cor);
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.WHITE));
+	}
+}
